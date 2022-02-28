83692d35f10b75eaab89bf65fbed6b3fa2437256
Suraj Nair <suraj2497@gmail.com>
2022-02-28 16:09:06 -0500

    Added Exception handling

--- HW03_Suraj_Nair_dictionary.py
+++ HW03_Suraj_Nair_dictionary.py
@@ -12,22 +12,30 @@ class Dictionary:
         return os.path.exists(file_path)
 
     def load_dictionary(self) -> str:
-        '''Reads the words text file and returns a list of valid words along with a random word'''
-        if not self.check_file_exists('new_words.txt'):
-            return '', []
-        words_file = open("new_words.txt", "r")
-        self.words = words_file.read().splitlines()
-        words_file.close()
-        if len(self.valid_words) == len(self.words):
-            self.valid_words.clear()
-        game_word = random.choice(self.words)
-        while game_word in self.valid_words:
+        try:
+            '''Reads the words text file and returns a list of valid words along with a random word'''
+            if not self.check_file_exists('new_words.txt'):
+                return ''
+            words_file = open("new_words.txt", "r")
+            self.words = words_file.read().splitlines()
+            words_file.close()
+            if len(self.valid_words) == len(self.words):
+                self.valid_words.clear()
             game_word = random.choice(self.words)
-        self.valid_words.append(game_word)
-        return game_word
+            while game_word in self.valid_words:
+                game_word = random.choice(self.words)
+            self.valid_words.append(game_word)
+            return game_word
+        except:
+            print('Dictionary could not be loaded')
+            return ''
 
     def check_valid_word(self, word: str) -> bool:
         '''Compares the given word with the words list to check if it exists'''
-        if not self.words or not word:
+        try:
+            if not self.words or not word:
+                return False
+            return word.lower() in self.words
+        except:
+            print('Word could not be checked')
             return False
-        return word.lower() in self.words
--- HW03_Suraj_Nair_ui.py
+++ HW03_Suraj_Nair_ui.py
@@ -4,31 +4,38 @@ from HW03_Suraj_Nair_logging import Logger
 
 def get_user_input_recur(attempt_no: int, total_attempts: int, attempted_words: list, dictionary: Dictionary, logger) -> str:
     '''Gets input word from user until valid'''
-    valid = False
-    # Keep getting input from user until it's a unique 5 letter word
-    while not valid:
-        valid, word = get_user_input(attempt_no, total_attempts,
-                                     attempted_words, dictionary, logger)
-    return word
+    try:
+        valid = 0  # 0 - False, 1- True, 2- Exit
+        # Keep getting input from user until it's a unique 5 letter word
+        while valid == 0:
+            valid, word = get_user_input(attempt_no, total_attempts,
+                                         attempted_words, dictionary, logger)
+        return word, valid
+    except:
+        print('Error in user input')
+        return ''
 
 
 def get_user_input(attempt_no: int, total_attempts: int, attempted_words: list, dictionary: Dictionary, logger: Logger) -> tuple[bool, str]:
     '''Gets input word from user'''
-    print(
-        f'Attempt {attempt_no} / {total_attempts} --> Please enter a 5 letter word:')
-    user_input = input().strip().upper()
-    if not user_input:
-        print('Exiting game!')
-        logger.write_log('User Exited the game\n')
-        logger.close_log()
-        quit()
-    valid = user_input_validation(
-        user_input, attempted_words, dictionary)
-    if valid:
-        logger.write_log(f'User Input #{attempt_no}: {user_input}\n')
-    else:
-        logger.write_log(f'Invalid User Input #{attempt_no}: {user_input}\n')
-    return valid, user_input
+    try:
+        print(
+            f'Attempt {attempt_no} / {total_attempts} --> Please enter a 5 letter word:')
+        user_input = input().strip().upper()
+        if not user_input:
+            print('Exiting game!')
+            logger.write_log('User Exited the game\n')
+            logger.close_log()
+            return 2, ''
+        valid = user_input_validation(user_input, attempted_words, dictionary)
+        if valid:
+            logger.write_log(f'User Input #{attempt_no}: {user_input}\n')
+        else:
+            logger.write_log(
+                f'Invalid User Input #{attempt_no}: {user_input}\n')
+        return int(valid), user_input
+    except Exception as e:
+        print(e)
 
 
 def user_input_validation(user_input, attempted_words, dictionary: Dictionary):
